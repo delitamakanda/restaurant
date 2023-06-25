710e76557189b925eb1da6d0a1c5ff849727ee68
Délita Makanda <delita.makanda@gmail.com>
2023-06-25 23:51:49 +0200

    chore(middleware): performance metrics

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'restaurant.middleware.metric_middleware',
 ]
 
 ROOT_URLCONF = 'restaurant.urls'
