6e547fd2a4db7b5bff08c57abe6fe459e8e41810
Délita Makanda <delita.makanda@gmail.com>
2024-06-07 01:51:15 +0200

    fix env

--- restaurant/settings_production.py
+++ restaurant/settings_production.py
@@ -4,7 +4,7 @@ from google.cloud import secretmanager
 
 def get_secret(secret_id):
     client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{os.environ.get('GOOGLE_CLOUD_PROJECT')}/secrets/{secret_id}/versions/latest"
+    name = f"projects/{os.environ['GOOGLE_CLOUD_PROJECT']}/secrets/{secret_id}/versions/latest"
     response = client.access_secret_version(name=name)
     return response.payload.data.decode("UTF-8")
 
